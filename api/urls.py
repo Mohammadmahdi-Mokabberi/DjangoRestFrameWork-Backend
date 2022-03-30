21c6e9437da6a0068e33767166b24b6fcf68b9d9
Mohammadmahdi-Mokabberi <mm.mokabberi@gmail.com>
2022-03-30 12:33:39 +0430

    mohammadmahdi:jwt added

--- api/urls.py
+++ api/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from .views import AlbumAPIView, AlbumDetailAPIView, AboutAPIView, StoryAPIView, StoryDetailAPIView, VideoAPIView, VideoDetailAPIView
 
 urlpatterns = [
-    path('album', AlbumAPIView.as_view()),
-    path('album/<int:pk>', AlbumDetailAPIView.as_view()),
-    path('story', StoryAPIView.as_view()),
-    path('story/<int:pk>', StoryDetailAPIView.as_view()),
-    path('video', VideoAPIView.as_view()),
-    path('video/<int:pk>', VideoDetailAPIView.as_view()),
-    path('about',AboutAPIView.as_view()),
+    path('album/', AlbumAPIView.as_view()),
+    path('album/<int:pk>/', AlbumDetailAPIView.as_view()),
+    path('story/', StoryAPIView.as_view()),
+    path('story/<int:pk>/', StoryDetailAPIView.as_view()),
+    path('video/', VideoAPIView.as_view()),
+    path('video/<int:pk>/', VideoDetailAPIView.as_view()),
+    path('about/',AboutAPIView.as_view()),
 ]
--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework_simplejwt import views as jwt_views
 
 
 schema_view = get_schema_view(
@@ -34,6 +35,8 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('api/login/', jwt_views.TokenObtainPairView.as_view(), name ='token_obtain_pair'),
+    path('api/refresh/', jwt_views.TokenRefreshView.as_view(), name ='token_refresh'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

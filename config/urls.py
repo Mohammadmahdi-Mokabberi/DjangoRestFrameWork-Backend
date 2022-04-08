4dc2c37ed0343db6472ced39366c6873b837f36c
Mohammadmahdi-Mokabberi <mm.mokabberi@gmail.com>
2022-04-08 13:33:59 +0430

    some changes

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from rest_framework_simplejwt import views
+
 
 
 schema_view = get_schema_view(
@@ -36,8 +36,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('user/', include('user.urls')),
-    path('api/login/', views.TokenObtainPairView.as_view(), name ='token_obtain_pair'),
-    path('api/refresh/', views.TokenRefreshView.as_view(), name ='token_refresh'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from rest_framework_simplejwt import views
 from .views import ChangePasswordAPIView, UserRegisterAPIView
 
 
 urlpatterns=[
     path('register/', UserRegisterAPIView.as_view()),
+    path('login/', views.TokenObtainPairView.as_view(), name ='token_obtain_pair'),
+    path('refresh/', views.TokenRefreshView.as_view(), name ='token_refresh'),
     path('change-password/', ChangePasswordAPIView.as_view()),
 ]
